0be9e9ffa827419ecf73931d87a26e8357705e6c
Jesus CC <jesuscc980203@gmail.com>
2022-10-27 11:45:55 -0700

    Arreglar error de tamaño del about

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,5 +1,26 @@
 function loadNavigation(){
-    document.getElementById("router-index").href = "http://127.0.0.1:5500" ;
+    // Router para servidor
+    // document.getElementById("router-index").href = "https://univafu-ng.000webhostapp.com.html" ;
+    // document.getElementById("router-secundaria").href = "https://univafu-ng.000webhostapp.com/colegio-valle-fuerte/Secundaria.html" ;
+    // document.getElementById("router-prepa-hs").href = "https://univafu-ng.000webhostapp.com/prepa-univafu/higiene-salud.html" ;
+    // document.getElementById("router-prepa-ti").href = "https://univafu-ng.000webhostapp.com/prepa-univafu/tecnologias.html" ;
+    // document.getElementById("router-uni-lnut").href = "https://univafu-ng.000webhostapp.com/univafu-universidad/licenciaturas/nutricion.html" ;
+    // document.getElementById("router-uni-gtyg").href = "https://univafu-ng.000webhostapp.com/univafu-universidad/licenciaturas/gestion-turistica-gastronomia.html" ;
+    // document.getElementById("router-uni-tfyr").href = "https://univafu-ng.000webhostapp.com/univafu-universidad/licenciaturas/terapia-fisica.html" ;
+    // document.getElementById("router-uni-tayl").href = "https://univafu-ng.000webhostapp.com/univafu-universidad/licenciaturas/terapia-audicion.html" ;
+    // document.getElementById("router-uni-lmkt").href = "https://univafu-ng.000webhostapp.com/univafu-universidad/licenciaturas/marketing-digital.html" ;
+
+    // document.getElementById("router-uni-mdch").href = "https://univafu-ng.000webhostapp.com/univafu-universidad/posgrados/desarrollo-capital-humano.html" ;
+    // document.getElementById("router-uni-mnch").href = "https://univafu-ng.000webhostapp.com/univafu-universidad/posgrados/nutricion-clinica.html" ;
+    // document.getElementById("router-uni-epyr").href = "https://univafu-ng.000webhostapp.com/univafu-universidad/posgrados/especialidad-prevencion.html" ;
+
+    // document.getElementById("router-fundacion-cli").href = "https://univafu-ng.000webhostapp.com/fundacion-univafu/clinica"
+    
+    // document.getElementById("router-idiomas").href = "https://univafu-ng.000webhostapp.com/univafu-idiomas/idiomas.html" ;
+    // document.getElementById("router-privacidad").href = "https://univafu-ng.000webhostapp.com/avisoPrivacidad.html" ;
+
+
+        document.getElementById("router-index").href = "http://127.0.0.1:5500/index.html" ;
     document.getElementById("router-secundaria").href = "http://127.0.0.1:5500/colegio-valle-fuerte/Secundaria.html" ;
     document.getElementById("router-prepa-hs").href = "http://127.0.0.1:5500/prepa-univafu/higiene-salud.html" ;
     document.getElementById("router-prepa-ti").href = "http://127.0.0.1:5500/prepa-univafu/tecnologias.html" ;
@@ -13,7 +34,7 @@ function loadNavigation(){
     document.getElementById("router-uni-mnch").href = "http://127.0.0.1:5500/univafu-universidad/posgrados/nutricion-clinica.html" ;
     document.getElementById("router-uni-epyr").href = "http://127.0.0.1:5500/univafu-universidad/posgrados/especialidad-prevencion.html" ;
 
-    document.getElementById("router-fundacion-cli").href = "http://127.0.0.1:5500/fundacion-univafu/clinica.html"
+    document.getElementById("router-fundacion-cli").href = "http://127.0.0.1:5500/fundacion-univafu/clinica"
     
     document.getElementById("router-idiomas").href = "http://127.0.0.1:5500/univafu-idiomas/idiomas.html" ;
     document.getElementById("router-privacidad").href = "http://127.0.0.1:5500/avisoPrivacidad.html" ;

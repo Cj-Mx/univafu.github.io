a57e505570d2c4a79195b176a58f09de904951fc
Jesus CC <jesuscc980203@gmail.com>
2022-10-31 12:36:20 -0700

    Remove link bootstrap

---
+++ assets/js/carga-academica.js
@@ -0,0 +1,112 @@
+let cargaAcademica = [
+    {
+        educacion: 'lmkt',
+        semestres: [
+            {
+                semestre: "1er Semestre",
+                cargaAcad: [
+                    "Comunicación Visual",
+                    "Taller de Creatividad",
+                    "Fundamentos de Marketing",
+                    "Administración",
+                    "Matemáticas Orientadas a los Negocios",
+                    "Taller de Expresión Oral y Escrita",
+                    "Valores Humanos",
+                    "Inglés I"
+                ]
+            }
+        ]
+    },
+    {
+        educacion: 'lmkt',
+        semestres: [
+            {
+                semestre: "1er Semestre",
+                cargaAcad: [
+                    "Comunicación Visual",
+                    "Taller de Creatividad",
+                    "Fundamentos de Marketing",
+                    "Administración",
+                    "Matemáticas Orientadas a los Negocios",
+                    "Taller de Expresión Oral y Escrita",
+                    "Valores Humanos",
+                    "Inglés I"
+                ]
+            }
+        ]
+    },
+
+    {
+        educacion: 'lmkt',
+        semestres: [
+            {
+                semestre: "1er Semestre",
+                cargaAcad: [
+                    "Comunicación Visual",
+                    "Taller de Creatividad",
+                    "Fundamentos de Marketing",
+                    "Administración",
+                    "Matemáticas Orientadas a los Negocios",
+                    "Taller de Expresión Oral y Escrita",
+                    "Valores Humanos",
+                    "Inglés I"
+                ]
+            }
+        ]
+    },
+    {
+        educacion: 'lmkt',
+        semestres: [
+            {
+                semestre: "1er Semestre",
+                cargaAcad: [
+                    "Comunicación Visual",
+                    "Taller de Creatividad",
+                    "Fundamentos de Marketing",
+                    "Administración",
+                    "Matemáticas Orientadas a los Negocios",
+                    "Taller de Expresión Oral y Escrita",
+                    "Valores Humanos",
+                    "Inglés I"
+                ]
+            }
+        ]
+    },
+    {
+        educacion: 'lmkt',
+        semestres: [
+            {
+                semestre: "1er Semestre",
+                cargaAcad: [
+                    "Comunicación Visual",
+                    "Taller de Creatividad",
+                    "Fundamentos de Marketing",
+                    "Administración",
+                    "Matemáticas Orientadas a los Negocios",
+                    "Taller de Expresión Oral y Escrita",
+                    "Valores Humanos",
+                    "Inglés I"
+                ]
+            }
+        ]
+    },
+    {
+        educacion: 'lmkt',
+        semestres: [
+            {
+                semestre: "1er Semestre",
+                cargaAcad: [
+                    "Comunicación Visual",
+                    "Taller de Creatividad",
+                    "Fundamentos de Marketing",
+                    "Administración",
+                    "Matemáticas Orientadas a los Negocios",
+                    "Taller de Expresión Oral y Escrita",
+                    "Valores Humanos",
+                    "Inglés I"
+                ]
+            }
+        ]
+    },
+
+]

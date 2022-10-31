46b036ac81a3206d27f0c25c0311bc326c3b57ee
Jesus CC <jesuscc980203@gmail.com>
2022-10-31 13:39:05 -0700

    Add academic charge lmkt

--- assets/js/carga-academica.js
+++ assets/js/carga-academica.js
@@ -1,27 +1,110 @@
 let cargaAcademica = [
     {
-        educacion: 'lmkt',
+        educacion: 'lgtyg',
         semestres: [
             {
                 semestre: "1er Semestre",
                 cargaAcad: [
-                    "Comunicación Visual",
-                    "Taller de Creatividad",
-                    "Fundamentos de Marketing",
+                    "Turismo y Medio Ambiente",
+                    "Gastronomía y Manejo Higiénico",
                     "Administración",
-                    "Matemáticas Orientadas a los Negocios",
+                    "Matemáticas Básicas",
+                    "Lenguaje y Cultura",
                     "Taller de Expresión Oral y Escrita",
-                    "Valores Humanos",
+                    "Tecnologías de Información y Comunicación (TIC'S)",
+                    "Valores Humanos"
+                ]
+            },
+            {
+                semestre: "2do Semestre",
+                cargaAcad: [
+                    "Planificación del Turismo",
+                    "Geografía Nacional",
+                    "Técnicas Culinarias",
+                    "Nutrición",
+                    "Contabilidad",
+                    "Análisis de la Información",
                     "Inglés I"
+                ]
+            },
+            {
+                semestre: "3er Semestre",
+                cargaAcad: [
+                    "Gestión del Hospedaje",
+
+                    "Geografía Mundial",
+
+                    "Panadería y Repostería",
+
+                    "Enología y Licores",
+
+                    "Estadística",
+
+                    "Metodología de la Investigación",
+
+                    "Inglés II"
+                ]
+            },
+            {
+                semestre: "4to Semestre",
+                cargaAcad: [
+                    "Patrimonio Turístico",
+                    "Historia de México",
+                    "Productos Cárnicos",
+                    "Cocina Vegetariana",
+                    "Administración de Recursos Humanos",
+                    "Calidad y Servicio al Cliente",
+                    "Desarrollo sustentable",
+                    "Inglés III"
+                ]
+            },
+            {
+                semestre: "5to Semestre",
+                cargaAcad: [
+                    "Circuitos Turísticos Nacionales e Internacionales",
+                    "Ecoturismo",
+                    "Cocina Mexicana",
+                    "Dietología",
+                    "Derecho y Legislación Turística",
+                    "Liderazgo",
+                    "Inglés IV"
+                ]
+            },
+            {
+                semestre: "6to Semestre",
+                cargaAcad: [
+                    "Organización de Eventos, Ferias y Exposiciones",
+                    "Cocina Internacional",
+                    "Economía y Finanzas",
+                    "Marketing y Comercialización"
+                ]
+            },
+            {
+                semestre: "7mo Semestre",
+                cargaAcad: [
+                    "Development of Management Skills",
+                    "Banquetes",
+                    "Cultura Emprendedora",
+                    "Francés I"
+                ]
+            },
+            {
+                semestre: "8vo Semestre",
+                cargaAcad: [
+                    "Taller de Guías Turísticos",
+                    "Taller de Creatividad Culinaria",
+                    "Desarrollo de Emprendedores",
+                    "Francés II",
+                    "Seminario de Trabajo de Investigación"
                 ]
             }
         ]
     },
     {
-        educacion: 'lmkt',
+        educacion: 'lnut',
         semestres: [
             {
-                semestre: "1er Semestre",
+                semestre: "8vo Semestre",
                 cargaAcad: [
                     "Comunicación Visual",
                     "Taller de Creatividad",
@@ -37,7 +120,25 @@ let cargaAcademica = [
     },
 
     {
-        educacion: 'lmkt',
+        educacion: 'ltal',
+        semestres: [
+            {
+                semestre: "1er Semestre",
+                cargaAcad: [
+                    "Comunicación Visual",
+                    "Taller de Creatividad",
+                    "Fundamentos de Marketing",
+                    "Administración",
+                    "Matemáticas Orientadas a los Negocios",
+                    "Taller de Expresión Oral y Escrita",
+                    "Valores Humanos",
+                    "Inglés I"
+                ]
+            }
+        ]
+    },
+    {
+        educacion: 'ltfyr',
         semestres: [
             {
                 semestre: "1er Semestre",
@@ -72,41 +173,4 @@ let cargaAcademica = [
             }
         ]
     },
-    {
-        educacion: 'lmkt',
-        semestres: [
-            {
-                semestre: "1er Semestre",
-                cargaAcad: [
-                    "Comunicación Visual",
-                    "Taller de Creatividad",
-                    "Fundamentos de Marketing",
-                    "Administración",
-                    "Matemáticas Orientadas a los Negocios",
-                    "Taller de Expresión Oral y Escrita",
-                    "Valores Humanos",
-                    "Inglés I"
-                ]
-            }
-        ]
-    },
-    {
-        educacion: 'lmkt',
-        semestres: [
-            {
-                semestre: "1er Semestre",
-                cargaAcad: [
-                    "Comunicación Visual",
-                    "Taller de Creatividad",
-                    "Fundamentos de Marketing",
-                    "Administración",
-                    "Matemáticas Orientadas a los Negocios",
-                    "Taller de Expresión Oral y Escrita",
-                    "Valores Humanos",
-                    "Inglés I"
-                ]
-            }
-        ]
-    },
-
 ]

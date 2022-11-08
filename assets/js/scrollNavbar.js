92fdb144d67bb2dddaaff4d1d6b6f1924fade99b
Jesus CC <jesuscc980203@gmail.com>
2022-11-08 08:24:10 -0800

    Mostrar navbar al deslizar hacia arriba

--- assets/js/scrollNavbar.js
+++ assets/js/scrollNavbar.js
@@ -1,11 +1,10 @@
 // script para mostrar el navbar de otro color despues de scroll
-
+var scrollP = window.scrollY;
 window.onscroll = function () {
     scrollFunction()
 };
 function scrollFunction() {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-
+    if (document.documentElement.scrollTop > 150 && window.scrollY < scrollP) { //Condicion para mostrar el navbar solo cuando se desplace hacia abajo
         document.getElementsByClassName("header-area")[0].style.backgroundColor = "black";
         document.getElementsByClassName("header-area")[0].style.position = "fixed";
         document.getElementsByClassName("header-area")[0].style.top = "0px";
@@ -14,4 +13,5 @@ function scrollFunction() {
         document.getElementsByClassName("header-area")[0].style.top = "50px";
         document.getElementsByClassName("header-area")[0].style.backgroundColor = "rgba(0, 0, 0, 0.15)"
     }
+    scrollP = window.scrollY;
 }

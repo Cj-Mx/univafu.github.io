bdae7b62482deeb64619630888949ef4a616b28f
Jesus CC <jesuscc980203@gmail.com>
2022-11-11 10:31:20 -0800

    Agregar un solo javascript para las funciones de scrollnavbar, progressbar y sendWhatsapp

--- assets/js/myFunctions.js
+++ assets/js/myFunctions.js
@@ -15,3 +15,30 @@ function scrollFunction() {
     }
     scrollP = window.scrollY;
 }
+
+
+// script para mostrar una barra de progreso
+
+let activeProgressBar = false;
+function reveal() {
+    var reveals = document.querySelectorAll(".no-progress");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("progress");
+            reveals[i + 1].classList.add("progress");
+            reveals[i + 2].classList.add("progress");
+            i = reveals.length;
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
+
+// script para enviar mensajes whatsapp
+function sendMessageWhatsapp(){
+    window.open('https://wa.me/573001112233?text=Hola!%20Estoy%20interesado%20en%20tu%20servicio', '_blank');
+}
--- assets/js/whatsapp.js
+++
@@ -1,3 +0,0 @@
-function sendMessageWhatsapp(){
-    window.open('https://wa.me/573001112233?text=Hola!%20Estoy%20interesado%20en%20tu%20servicio', '_blank');
-}

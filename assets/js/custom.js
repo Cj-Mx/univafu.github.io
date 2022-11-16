d0a7aba6e9fd4034aeaea4d97ed63ab7355af5f9
Jesus CC <jesuscc980203@gmail.com>
2022-11-16 08:39:23 -0800

    Eliminar codigo comentado

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,5 @@
 (function ($) {
-	
 	"use strict";
-
-	// $(window).scroll(function() {
-	//   var scroll = $(window).scrollTop();
-	//   var box = $('.header-text').height();
-	//   var header = $('header').height();
-
-	//   if (scroll >= box - header) {
-	//     $("header").addClass("background-header");
-	//   } else {
-	//     $("header").removeClass("background-header");
-	//   }
-	// });
-	
 	$('.filters ul li').click(function(){
         $('.filters ul li').removeClass('active');
         $(this).addClass('active');
@@ -35,9 +21,9 @@
 
 	const Accordion = {
 	  settings: {
-	    // Expand the first item by default
+	    // Expander el primer elemento por defecto
 	    first_expanded: false,
-	    // Allow items to be toggled independently
+	    // Permitir que los elementos se cambien de forma independiente
 	    toggle: false
 	  },
 
@@ -57,13 +43,13 @@
 	  init: function(el) {
 	    const _this = this;
 
-	    // Override default settings with classes
+	    // Anular la configuración predeterminada con clases
 	    let is_first_expanded = _this.settings.first_expanded;
 	    if (el.classList.contains("is-first-expanded")) is_first_expanded = true;
 	    let is_toggle = _this.settings.toggle;
 	    if (el.classList.contains("is-toggle")) is_toggle = true;
 
-	    // Loop through the accordion's sections and set up the click behavior
+	    // Recorra las secciones del acordeón y configure el comportamiento del clic
 	    const sections = el.getElementsByClassName("accordion");
 	    const all_toggles = el.getElementsByClassName("accordion-head");
 	    const all_contents = el.getElementsByClassName("accordion-body");
@@ -72,18 +58,18 @@
 	      const toggle = all_toggles[i];
 	      const content = all_contents[i];
 
-	      // Click behavior
+	      // Comportamiento de clics
 	      toggle.addEventListener("click", function(e) {
 	        if (!is_toggle) {
-	          // Hide all content areas first
+	          // Ocultar todas las áreas de contenido primeroOcultar todas las áreas de contenido primeroOcultar todas las áreas de contenido primero
 	          for (let a = 0; a < all_contents.length; a++) {
 	            _this.closeAccordion(all_toggles[a], all_contents[a]);
 	          }
 
-	          // Expand the clicked item
+	          // Expandir el elemento en el que se hizo clic
 	          _this.openAccordion(toggle, content);
 	        } else {
-	          // Toggle the clicked item
+	          // Alternar el elemento en el que se hizo clic
 	          if (toggle.classList.contains("is-open")) {
 	            _this.closeAccordion(toggle, content);
 	          } else {
@@ -92,7 +78,7 @@
 	        }
 	      });
 
-	      // Expand the first item
+	      // Expandir el primer elemento
 	      if (i === 0 && is_first_expanded) {
 	        _this.openAccordion(toggle, content);
 	      }
@@ -101,7 +87,7 @@
 	};
 
 	(function() {
-	  // Initiate all instances on the page
+	  // Iniciar todas las instancias de la página.
 	  const accordions = document.getElementsByClassName("accordions");
 	  for (let i = 0; i < accordions.length; i++) {
 	    Accordion.init(accordions[i]);
@@ -150,7 +136,7 @@
 	  })
 	
 
-	// Menu Dropdown Toggle
+	// Alternar menú desplegable
 	if($('.menu-trigger').length){
 		$(".menu-trigger").on('click', function() {	
 			$(this).toggleClass('active');
@@ -158,68 +144,7 @@
 		});
 	}
 
-
-	// Menu elevator animation
-	// $('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function() {
-	// 	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-	// 		var target = $(this.hash);
-	// 		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	// 		if (target.length) {
-	// 			var width = $(window).width();
-	// 			if(width < 991) {
-	// 				$('.menu-trigger').removeClass('active');
-	// 				$('.header-area .nav').slideUp(200);	
-	// 			}				
-	// 			$('html,body').animate({
-	// 				scrollTop: (target.offset().top) - 80
-	// 			}, 700);
-	// 			return false;
-	// 		}
-	// 	}
-	// });
-
-	// $(document).ready(function () {
-	//     $(document).on("scroll", onScroll);
-	    
-	//     //smoothscroll
-	//     $('.scroll-to-section a[href^="#"]').on('click', function (e) {
-	//         e.preventDefault();
-	//         $(document).off("scroll");
-	        
-	//         $('.scroll-to-section a').each(function () {
-	//             $(this).removeClass('active');
-	//         })
-	//         $(this).addClass('active');
-	      
-	//         var target = this.hash,
-	//         menu = target;
-	//        	var target = $(this.hash);
-	//         $('html, body').stop().animate({
-	//             scrollTop: (target.offset().top) - 79
-	//         }, 500, 'swing', function () {
-	//             window.location.hash = target;
-	//             $(document).on("scroll", onScroll);
-	//         });
-	//     });
-	// });
-
-	// function onScroll(event){
-	//     var scrollPos = $(document).scrollTop();
-	//     $('.nav a').each(function () {
-	//         var currLink = $(this);
-	//         var refElement = $(currLink.attr("href"));
-	//         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-	//             $('.nav ul li a').removeClass("active");
-	//             currLink.addClass("active");
-	//         }
-	//         else{
-	//             currLink.removeClass("active");
-	//         }
-	//     });
-	// }
-
-
-	// Page loading animation
+	// Animación de carga de la página.
 	$(window).on('load', function() {
 		if($('.cover').length){
 			$('.cover').parallax({
@@ -241,7 +166,7 @@
 
 	const dropdownOpener = $('.main-nav ul.nav .has-sub > a');
 
-    // Open/Close Submenus
+    // Abrir/Cerrar submenus
     if (dropdownOpener.length) {
         dropdownOpener.each(function () {
             var _this = $(this);

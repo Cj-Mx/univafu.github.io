47c83117ef7b7b283c9c2da0d742a2be42a65968
Jesus CC <jesuscc980203@gmail.com>
2022-12-14 10:09:20 -0800

    Preparar para servidor

--- assets/js/sendEmail.php
+++ assets/js/sendEmail.php
@@ -9,14 +9,10 @@ $cuerpo = "Mensaje: \n" . $data['mensaje'] . "\n\n\n" . "Información de contact
           "Telefono: " . $data['telefono'] . "\n";
 $txt = $data['mensaje'] . '\r\n' . ' Información de contacto:  ' . '\r\n' . 'Nombre: ' . $data['nombre'] . '\r\n' . 'Telefono: ' . $data['telefono'] . '\r\n' . 'Correo Electronico ' . $data['email'];
 $headers = "From: " . 'info@univafu.edu.mx';
-mail($to,$subject,$cuerpo,$headers);
-# $data = json_decode(file_get_contents('php://input'), true);
- #echo $data['nombre'];
-#$to = $data['emailTo'];
-#$subject = "My subject";
-#$txt = $data['nombre'];
-#$headers = "From: webmaster@example.com" . "\r\n" .
-#"CC: us-imm-node6c.000webhost.io";
-#mail($to,$subject,$txt,$headers);
-
+if(mail($to,$subject,$cuerpo,$headers)){
+    echo "Mensaje enviado con exito";
+}
+else{
+    echo "Error";
+}
 ?>
--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,16 +1,17 @@
 
 
-function enviarCorreo() {
+function enviarCorreo(emailTo) {
     params = {
          nombre: document.getElementById('nombre').value,
          email : document.getElementById('email').value,
          telefono : document.getElementById('telefono').value,
-         mensaje : document.getElementById('mensaje').value
+         mensaje : document.getElementById('mensaje').value,
+         emailTo : emailTo
     }
     var nombre = document.getElementById('nombre').value;
     console.log(params);
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST", "https://pagetest98.000webhostapp.com/assets/js/sendEmail.php", true);
+    xmlhttp.open("POST", "https://pagetest398.000webhostapp.com/assets/js/sendEmail.php", true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.onreadystatechange = function () {
         if (this.readyState === 4 || this.status === 200) {

d1fd380f5ea27ecbeb7203e04e181a810c24e4c3
Jesus CC <jesuscc980203@gmail.com>
2022-12-20 08:51:32 -0800

    Arreglar error de alt de las imagenes de carga academica

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -49,7 +49,7 @@ function loadNavigation() {
     // console.log(window.location.pathname.split('/'));
 }
 
-function modalShow(){
+function modalShow() {
     var modal = document.getElementById("myModal");
 
     // Get the image and insert it inside the modal - use its "alt" text as a caption
@@ -61,16 +61,16 @@ function modalShow(){
         modalImg.src = this.src;
         captionText.innerHTML = this.alt;
     }
-    
+
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close-plan")[0];
-    
+
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
     }
-    
-    
+
+
 }
 
 
